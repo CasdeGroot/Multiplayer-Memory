1e43cf62cab992c5bded3b6c6c7095d8f6078d48
casdeGroot <casdegroot@live.nl>
2016-06-08 11:46:44 +0200

    filled Card class

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,81 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 /**
  * Created by Joshua on 8-6-2016.
  */
-public class Card
+public class Card implements Comparable<Card>
 {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        BufferedImage back;
+        BufferedImage front;
+        CardImage state;
+        boolean isFlipped;
 
-        public Card()
+        public Card(int x, int y, int width, int height, String url, CardImage state)
         {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+                isFlipped = false;
 
+                try
+                {
+                        back = ImageIO.read(new File(url));
+                        front = ImageIO.read(new File("res//front.png"));
+                }
+                catch(Exception e)
+                {
+                        e.printStackTrace();
+                }
+        }
+
+        public void flip()
+        {
+                isFlipped =! isFlipped;
+        }
+
+
+        @Override
+        public int compareTo(Card o)
+        {
+                if(this.state == o.getImage())
+                {
+                        return 1;
+                }
+                else
+                {
+                        return 0;
+                }
+        }
+
+        public CardImage getImage()
+        {
+                return state;
+        }
+
+        public void onClick(int x,int y)
+        {
+                if(x > this.x && x < this.x + width)
+                {
+                        if(y > this.y && y < this.y + width)
+                        {
+                                flip();
+                        }
+                }
+        }
+
+        public void draw(Graphics2D g2)
+        {
+                if(!isFlipped)
+                        g2.drawImage(front,x,y,null);
+                else
+                        g2.drawImage(back, x,y,null);
         }
 }
---
+++ src/CardImage.java
@@ -0,0 +1,7 @@
+/**
+ * Created by casde on 8-6-2016.
+ */
+public enum CardImage
+{
+    ONE, TWO, THREE, FOUR,FIVE,SIX,SEVEN,EIGHT
+}
