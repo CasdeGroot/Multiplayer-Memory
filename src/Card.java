fd166f357c5c7fcfd4caa73b60197b12ddb7fb21
Joshua <jlatusia@hotmail.com>
2016-06-08 11:32:52 +0200

    not

---
+++ src/Card.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Joshua on 8-6-2016.
+ */
+public class Card
+{
+
+        public Card()
+        {
+
+        }
+}
---
+++ src/Client.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Joshua on 8-6-2016.
+ */
+public class Client {
+}
